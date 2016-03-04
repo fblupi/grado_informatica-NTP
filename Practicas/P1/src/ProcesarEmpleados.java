d279e722201e3e0a51774b7658ee408682f33eb7
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-03-04 10:45:39 +0100

    Creando y usando predicados

--- Practicas/P1/src/ProcesarEmpleados.java
+++ Practicas/P1/src/ProcesarEmpleados.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * Created by FranciscoJavier on 04/03/2016.
@@ -15,7 +18,26 @@ public class ProcesarEmpleados {
                 new Empleado("Pedro", "Garcia", 3200, "Ventas"),
                 new Empleado("Fernado", "Gonzalez", 4236.4, "Marketing")};
 
-        List<Empleado> lista = Arrays.asList(empleados);
-        
+        List<Empleado> lista = Arrays.asList(empleados); // paso a lista
+        Stream<Empleado> flujo = lista.stream(); // paso a flujo
+
+        System.out.printf("%nLista completa de empleados:%n");
+        flujo.forEach(System.out::println); // necesario toString en la clase empleado
+
+        Predicate<Empleado> condicion = // evito tener que definir un método
+                empleado -> (empleado.obtenerSueldo() >= 4000 &&
+                             empleado.obtenerSueldo() <= 6000);
+
+        System.out.printf("%nEmpleados seleccionados y ordenados:%n");
+        lista.stream()
+                .filter(condicion)
+                .sorted(Comparator.comparing(Empleado::obtenerSueldo))
+                .forEach(System.out::println);
+
+        System.out.printf("%nPrimer empleado de los seleccionados:%n%s%n",
+                lista.stream()
+                    .filter(condicion)
+                    .findFirst()
+                    .get());
     }
 }
