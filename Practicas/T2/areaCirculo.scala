4da720d09fe159f738f34c94359ed14ed59f0d71
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-03-14 10:15:54 +0100

    Script para calcular el area de un circulo mejorado

--- Practicas/T2/areaCirculo.scala
+++ Practicas/T2/areaCirculo.scala
@@ -1,7 +1,13 @@
-def areaCirculo(r: Double) : Double = math.Pi * math.pow(r, 2)
+//def areaCirculo(r: Double) : Double = math.Pi * math.pow(r, 2)
 //def areaCirculo(r: Double) = math.Pi * math.pow(r, 2)
+def areaCirculo(r: String) : Double = {
+	r.isEmpty match {
+		case true => 0
+		case false => math.Pi * math.pow(r.toDouble, 2)
+	}
+}
 
 if (args.length > 0)
-	println(areaCirculo(args(0).toDouble))
+	println(areaCirculo(args(0)))
 else 
 	Console.err.println("Introduzca radio del círculo")
