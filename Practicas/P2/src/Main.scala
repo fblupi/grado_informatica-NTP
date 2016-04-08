d9492b1a91d124ac4c663b4cedd96a6e2420c0d7
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-08 12:11:38 +0200

    Contar cambios posibles

--- Practicas/P2/src/Main.scala
+++ Practicas/P2/src/Main.scala
@@ -13,17 +13,14 @@ object Main {
     */
   def main(args: Array[String]) {
     println("................... Triangulo de Pascal ...................")
-
     // Se muestran 10 filas del trinagulo de Pascal
     for (row <- 0 to 10) {
       // Se muestran 10 10 columnas
       for (col <- 0 to row)
         print(calcularValorTrianguloPascal(col, row) + " ")
-
       // Salto de linea final para mejorar la presentacion
       println()
     }
-
     // Se muestra el valor que debe ocupar la columna 5 en la fila 10
     println(calcularValorTrianguloPascal(10, 15))
     println(calcularValorTrianguloPascal(0, 0))
@@ -72,7 +69,8 @@ object Main {
     * @return contador de numero de vueltas posibles
     */
   def contarCambiosPosibles(cantidad: Int, monedas: List[Int]): Int = {
-    // A rellenar
-    4
+    if (cantidad == 0) 1
+    else if (cantidad < 0 || monedas.isEmpty) 0
+    else contarCambiosPosibles(cantidad - monedas.head, monedas) + contarCambiosPosibles(cantidad, monedas.tail)
   }
 }
