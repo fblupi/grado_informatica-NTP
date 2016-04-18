e1dba702aaf4f0a4cb665841e74aaa3868a314b5
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-18 10:17:41 +0200

    Función para determinar si dos reinas están en jaque

---
+++ Teoria/T2/ochoReinas.scala
@@ -0,0 +1,4 @@
+def enJaque(reina1 : (Int, Int), reina2 : (Int, Int)) : Boolean
+  reina1._1 == reina2._1 ||
+  reina1._2 == reina2._2 ||
+  (reina1._1 - reina1._2).abs == (reina2._1 - reina2._2).abs
