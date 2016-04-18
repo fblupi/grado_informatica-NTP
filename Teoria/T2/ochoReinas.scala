71b4ce51d6bb04a120d523dcb79f1708f59fbc33
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-18 10:22:26 +0200

    Función para determinar si una reina está en jaque con otras

--- Teoria/T2/ochoReinas.scala
+++ Teoria/T2/ochoReinas.scala
@@ -1,4 +1,7 @@
-def enJaque(reina1 : (Int, Int), reina2 : (Int, Int)) : Boolean
+def enJaque(reina1 : (Int, Int), reina2 : (Int, Int)) : Boolean =
   reina1._1 == reina2._1 ||
   reina1._2 == reina2._2 ||
   (reina1._1 - reina1._2).abs == (reina2._1 - reina2._2).abs
+
+def posicionCorrecta(reina : (Int, Int), solucionParcial : List[(Int, Int)]) : Boolean =
+  solucionParcial forall(reinaPuesta => !enJaque(reina, reinaPuesta))
