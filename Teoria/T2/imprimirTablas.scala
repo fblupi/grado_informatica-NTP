aec5f769e0cfef975ac8f8d56a164a0d7873f55a
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-18 10:12:06 +0200

    Programación funcional

--- Teoria/T2/imprimirTablas.scala
+++ Teoria/T2/imprimirTablas.scala
@@ -2,9 +2,8 @@ def imprimirTablasFor =
   for (i <- 1 to 10) {
     for (j <- 1 to 10) {
       val prod = (i * j).toString
-      for (k <- prod.length until 4) {
+      for (k <- prod.length until 4)
         print(" ")
-      }
       print(prod)
     }
     println
@@ -29,4 +28,26 @@ def imprimirTablasWhile = {
   }
 }
 
+def generarTablaNumero(fila : Int) =
+  for (columna <- 1 to 10) yield {
+    val prod = (fila * columna).toString
+    val blank = " " * (4 - prod.length)
+    blank + prod
+  }
+
+def imprimirTablas = {
+  val tablas = for (fila <- 1 to 10) yield
+    generarTablaNumero(fila).mkString
+  tablas.mkString("\n")
+}
+
+/******************************************************************************/
+
+println("Usando While (No funcional)")
 imprimirTablasWhile
+println
+println("Usando For (No funcional)")
+imprimirTablasFor
+println
+println("Usando For (Funcional)")
+println(imprimirTablas)
