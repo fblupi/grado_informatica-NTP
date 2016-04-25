4ac30070870551772033a2e6c9162a5c39e1d83d
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-25 09:42:45 +0200

    Sumar potencias de dos de enteros consecutivos funcional

--- Teoria/T2/sumarEnteros.scala
+++ Teoria/T2/sumarEnteros.scala
@@ -8,4 +8,12 @@ def sumarCuadrados(desde: Int, hasta: Int): Int =
   if (desde > hasta) 0
   else elevarCuadrado(desde) + sumarCuadrados(desde + 1, hasta)
 
-println(sumarCuadrados(1, 3))
+def obtenerPotenciaDos(x: Int): Int =
+  if (x == 0) 1
+  else 2 * obtenerPotenciaDos(x - 1)
+
+def sumarPotenciasDos(desde: Int, hasta: Int): Int =
+  if (desde > hasta) 0
+  else obtenerPotenciaDos(desde) + sumarPotenciasDos(desde + 1, hasta)
+
+println(sumarPotenciasDos(1, 3))
