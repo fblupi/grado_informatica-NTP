634215db9ec1289320edaf10223d6786be446935
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-04-25 09:37:00 +0200

    Sumar cuadrados de enteros consecutivos funcional

--- Teoria/T2/sumarEnteros.scala
+++ Teoria/T2/sumarEnteros.scala
@@ -1,5 +1,11 @@
 def sumarEnteros(desde: Int, hasta: Int): Int =
   if (desde > hasta) 0
   else desde + sumarEnteros(desde + 1, hasta)
-  
-println(sumarEnteros(4, 8))
+
+def elevarCuadrado(x: Int) = x * x
+
+def sumarCuadrados(desde: Int, hasta: Int): Int =
+  if (desde > hasta) 0
+  else elevarCuadrado(desde) + sumarCuadrados(desde + 1, hasta)
+
+println(sumarCuadrados(1, 3))
