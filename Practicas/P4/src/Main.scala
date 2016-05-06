1a35b194a37929da3c7c3407676b3aad7f742576
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-05-06 20:36:01 +0200

    Main terminado #5 #6

--- Practicas/P4/src/Main.scala
+++ Practicas/P4/src/Main.scala
@@ -22,10 +22,18 @@ object TerminosGoogleApple {
   * Clase para probar la funcionalidad
   */
 object Main extends App {
+  // obtiene los mensajes que son comunes en ambos conjuntos
+  val mensajesComunes = TerminosGoogleApple.mensajesGoogle.interseccion(TerminosGoogleApple.mensajesApple)
+
+  // funcion recursiva auxiliar para obtener el tweet con mas RT de una tendencia, es decir, el ultimo
+  def obtenerRTUltimoMensajeDeTendencia(tendencia: Tendencia): Integer =
+    if (tendencia.tail.isEmpty) tendencia.head.retweets
+    else obtenerRTUltimoMensajeDeTendencia(tendencia.tail)
+
   print("1. Numero de mensajes en:\n")
-  print("\tGoogle: ")
+  print("Google: ")
   print(TerminosGoogleApple.mensajesGoogle.numeroMensajes)
-  print("\n\tApple: ")
+  print("\nApple: ")
   print(TerminosGoogleApple.mensajesApple.numeroMensajes)
   print("\n\n")
 
@@ -33,27 +41,25 @@ object Main extends App {
   print(TerminosGoogleApple.tendencia.length)
   print("\n\n")
 
-  val mensajesComunes = TerminosGoogleApple.mensajesGoogle.interseccion(TerminosGoogleApple.mensajesApple)
-
   print("3. Numero de mensajes comunes: ")
   print(mensajesComunes.numeroMensajes)
   print("\n\n")
 
   print("4. Orden de influencia de los mensajes comunes:\n")
   mensajesComunes.ordenacionAscendentePorRetweet.foreach(t => println(t))
-  print("\n\n")
+  print("\n")
 
   print("5. Maximo y minimo numero de retweets en los mensajes comunes:\n")
-  print("\tMaximo: ")
-  //print(mensajesComunes.ordenacionAscendentePorRetweet)
-  print("\n\tMinimo: ")
-  //print(mensajesComunes.ordenacionAscendentePorRetweet)
+  print("Maximo: ")
+  print(obtenerRTUltimoMensajeDeTendencia(mensajesComunes.ordenacionAscendentePorRetweet))
+  print("\nMinimo: ")
+  print(mensajesComunes.ordenacionAscendentePorRetweet.head.retweets)
   print("\n\n")
 
   print("6. Maximo y minimo de retweets en toda la coleccion de tendencia:\n")
-  print("\tMaximo: ")
-  //print(tendencia)
-  print("\n\tMinimo: ")
-  //print(tendencia)
+  print("Maximo: ")
+  print(obtenerRTUltimoMensajeDeTendencia(TerminosGoogleApple.tendencia))
+  print("\nMinimo: ")
+  print(TerminosGoogleApple.tendencia.head.retweets)
   print("\n\n")
 }
