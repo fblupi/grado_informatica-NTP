fb5bb15c1c6c5c1e6ce42b912e7a74d37f80dde8
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-05-28 11:19:49 +0200

    Esqueleto de generar árbol de codificación

--- Practicas/P5/src/Huffman.scala
+++ Practicas/P5/src/Huffman.scala
@@ -33,4 +33,55 @@ object Huffman {
   def generarArbol(nodoIzda: Nodo, nodoDcha: Nodo): Nodo =
     NodoIntermedio(nodoIzda, nodoDcha,
       obtenerCaracteres(nodoIzda) ::: obtenerCaracteres(nodoDcha), calcularPeso(nodoIzda) + calcularPeso(nodoDcha))
+
+  /**
+    * Dado un texto, calcula y construye un árbol de codificación analizando sus caracteres y contadores de ocurrencia
+    *
+    * @param caracteres
+    * @return
+    */
+  def generarArbolCodificacion(caracteres: List[Char]) : Nodo = ???
+
+  /**
+    * Calcula la frecuencia de aparición de cada carácter en el texto a analizar
+    *
+    * @param caracteres
+    * @return
+    */
+  def obtenerTuplasOcurrencias(caracteres: List[Char]) : List[(Char, Integer)] = ???
+
+  /**
+    * Genera una lista con toidos los nodos hoja del árbol de codificación. Esta lista de nodos terminales está
+    * ordenada por pesos de forma ascendente
+    *
+    * @param ocurrencias
+    * @return
+    */
+  def generarListHojasOrdenadas(ocurrencias: List[(Char, Integer)]) : List[NodoHoja] = ???
+
+  /**
+    * Comprueba si una lista de nodos contiene un único elemento
+    *
+    * @param nodos
+    * @return
+    */
+  def singleton(nodos: List[Nodo]) : Boolean = ???
+
+  /**
+    * Combina todos los nodos terminales
+    *
+    * @param nodos
+    * @return
+    */
+  def combinar(nodos: List[Nodo]) : List[Nodo] = ???
+
+  def hasta = ???
+
+  /**
+    * Pasar de cadena de texto normal a lista de caracteres
+    *
+    * @param cadena
+    * @return
+    */
+  def stringAListaCaracteres(cadena: String) :List[Char] = cadena.toList
 }
